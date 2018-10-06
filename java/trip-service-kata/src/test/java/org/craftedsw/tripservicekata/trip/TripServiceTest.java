2acf20bbcb5fb8371dd949750eae75af06e57f6b
Anna Rodriguez Gallego <anna.rodriguez@scmspain.com>
2018-10-06 12:55:11 +0200

    Add test for user not logged use case

--- java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
+++ java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
@@ -1,5 +1,24 @@
 package org.craftedsw.tripservicekata.trip;
 
+import org.craftedsw.tripservicekata.exception.UserNotLoggedInException;
+import org.craftedsw.tripservicekata.user.User;
+import org.junit.Test;
+
 public class TripServiceTest {
+
+  @Test(expected = UserNotLoggedInException.class)
+  public void when_user_not_logged_then_fails(){
+    TripServiceTestable tripService = new TripServiceTestable();
+    tripService.getTripsByUser(null);
+  }
+
+
+
+  class TripServiceTestable extends TripService{
+    @Override
+    protected User getLoggedUser() {
+      return null;
+    }
+  }
 	
 }
