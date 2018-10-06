7b251c9f7b48b6fb8fd28df98b90d4ac7ffe7a53
Anna Rodriguez Gallego <anna.rodriguez@scmspain.com>
2018-10-06 13:19:44 +0200

    Add test for users are not friends use case

--- java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
+++ java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
@@ -4,21 +4,46 @@ import org.craftedsw.tripservicekata.exception.UserNotLoggedInException;
 import org.craftedsw.tripservicekata.user.User;
 import org.junit.Test;
 
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+
 public class TripServiceTest {
 
   @Test(expected = UserNotLoggedInException.class)
-  public void when_user_not_logged_then_fails(){
-    TripServiceTestable tripService = new TripServiceTestable();
+  public void when_user_not_logged_then_fails() {
+    TripServiceTestable tripService = new TripServiceTestable(null);
     tripService.getTripsByUser(null);
   }
 
+  @Test
+  public void when_users_are_not_friends_then_no_trips_are_returned() {
+    User loggedUser = new User();
+    User requestedUser = new User();
+
+    TripServiceTestable tripService = new TripServiceTestable(loggedUser);
+    List<Trip> trips = tripService.getTripsByUser(requestedUser);
+
+    assertThat(trips).isEmpty();
+
+  }
+}
 
 
-  class TripServiceTestable extends TripService{
+
+   class TripServiceTestable extends TripService {
+    private final User user;
+
+    public TripServiceTestable(User user) {
+      this.user = user;
+    }
+
     @Override
     protected User getLoggedUser() {
-      return null;
+      return user;
     }
+
   }
+
 	
-}
+
